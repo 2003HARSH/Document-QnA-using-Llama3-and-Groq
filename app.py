c2e4ee860f221ff9433eb96d9ebbe04cf3cf0c06
Harsh Gupta <97147437+2003HARSH@users.noreply.github.com>
2024-05-17 11:25:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from langchain_utils import get_pdf_text, get_text_chunks, get_vector_store,user_input
+
+st.set_page_config(page_icon='📚',
+                   page_title="Document📃 QnA🙋‍♂️ using Groq",                  
+)
+
+GOOGLEPALM_API_KEY=st.secrets['GOOGLEPALM_API_KEY']
+GROQ_API_KEY=st.secrets['GROQ_API_KEY']
+
+def main():
+    st.header("Document📃 QnA💁 using Llama3 and Groq⏩")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        if st.button('Generate Answer'):
+            output=user_input(user_question,GROQ_API_KEY)
+            st.write(output[0])
+            document=output[1]
+            for i in range(len(document)):
+                st.markdown("""---""")
+                st.write("Source Document:",document[i].metadata['source'])
+                st.write("From Page Number:",document[i].metadata['page'])
+
+    with st.sidebar:
+        st.title("Upload your PDF files here:")
+        pdf_docs = st.file_uploader("You may upload multiple files. Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                docs = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(docs)
+                get_vector_store(text_chunks,GOOGLEPALM_API_KEY)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ langchain_utils.py
@@ -0,0 +1,72 @@
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import GooglePalmEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain.chains.question_answering import load_qa_chain
+import tempfile
+
+
+def get_pdf_text(pdf_docs):
+    docs=[]
+    for pdf in pdf_docs:
+        temp_dir = tempfile.mkdtemp()
+        path = os.path.join(temp_dir, pdf.name)
+        with open(path, "wb") as f:
+                f.write(pdf.getvalue())
+        loader = PyPDFLoader(path)
+        docs=docs+loader.load_and_split()
+    print(docs)
+    return  docs
+
+def get_text_chunks(docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_documents(docs)
+    return chunks
+
+def get_vector_store(docs,GOOGLEPALM_API_KEY):
+    load_dotenv()
+    embeddings = GooglePalmEmbeddings(google_api_key=GOOGLEPALM_API_KEY)
+    vector_store = FAISS.from_documents(docs, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+    print('got vector stores')
+
+def get_conversational_chain(GROQ_API_KEY):
+    load_dotenv()
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    llm=ChatGroq(groq_api_key=GROQ_API_KEY,
+                 model_name='Llama3-8b-8192')
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(llm, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+def user_input(user_question,GROQ_API_KEY):
+    load_dotenv()
+    embeddings = GooglePalmEmbeddings(google_api_key=os.getenv('GOOGLEPALM_API_KEY'))
+    
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question,k=2)
+
+    chain = get_conversational_chain(GROQ_API_KEY)
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+    
+    return[response,docs]
+
+
